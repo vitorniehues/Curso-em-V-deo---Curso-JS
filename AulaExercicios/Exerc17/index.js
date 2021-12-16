df542bb6ca9657da0256559c466669de077c4172
Vitor Niehues <niehues.vitor@gmail.com>
2021-12-16 12:21:03 -0300

    E17 Geração de Tauada

---
+++ AulaExercicios/Exerc17/index.js
@@ -0,0 +1,12 @@
+function geraTabuada() {
+    const num = Number(document.getElementById('txt1').value)
+    const slc = document.getElementById('slc')
+    if (num == 0) document.getElementById('txt1').value = 0
+
+    slc.replaceChildren();
+    for (let i = 0; i <= 10 ; i++) {
+        let opt = document.createElement('option')
+        opt.innerHTML = `${num} X ${i} = ${i*num}`
+        slc.appendChild(opt);
+ }
+}
