249e6b0520da57167155e1f37159f81508f49524
Vitor Niehues <niehues.vitor@gmail.com>
2021-12-16 11:50:30 -0300

    E16 Correção de bug e validação de números

--- AulaExercicios/Exerc16/index.js
+++ AulaExercicios/Exerc16/index.js
@@ -1,16 +1,23 @@
 function contar() {
-    let txt1 = document.getElementById('txt1').value;
-    let txt2 = document.getElementById('txt2').value;
-    let txt3 = document.getElementById('txt3').value;
-    let ini = Number(txt1);
-    let fim = Number(txt2);
-    let passo = Number(txt3);
-
-    
+    let ini   = Number(document.getElementById('txt1').value);
+    let fim   = Number(document.getElementById('txt2').value);
+    let passo = Number(document.getElementById('txt3').value);
     let res = document.getElementById('res')
+    let err = '';
+    
     res.innerHTML = ''
 
-    for (let i = txt1; i < txt2; i += txt3) {
-        res.innerHTML += i + ' '
+    if (ini == 0) document.getElementById('txt1').value = 0 
+    if (fim == 0) document.getElementById('txt2').value = 0 
+    if (ini > fim)          err += 'Erro! Inicio maior que fim.<br>'
+    if (passo == 0)         err += 'Erro! Passo em branco.<br>'
+    if (fim - ini < passo)  err += 'Erro! Passo maior que o intervalo.<br>'
+    
+    if (err !== '') return res.innerHTML = err
+
+    for (let i = ini; i < fim; i += passo) {
+        res.innerHTML += i;
+        if ( i + 1 < fim)  res.innerHTML += ', '
     }
+    res.innerHTML += '!'
 }
